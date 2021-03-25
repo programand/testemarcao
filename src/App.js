2417e173687b49af744ef1fe4ba639a2b8f8b3f8
programand <yara.yyg@gmail.com>
2021-03-25 21:53:00 +0000

    editada a estilização

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
+function Todo({ todo}){
+    return (
+        <div className="todo">
+            {todo.text}
+        </div>
+    );
+};
+
 function App() {
+        const [todos, setTodos] = React.useState([
+            { text: "Fazer as atividades da quinzena"},
+            { text: "Estudar para PW3"},
+            { text: "Marcar reunião para o TCC"},
+            { text: "Realizar os exercícios"}
+        ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Meu aplicativo React  no firebase</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="todo-list">
+         {todos.map((todo, index) => ( // percorrer os todos e criando um todo para cada da lista
+             <Todo 
+                key={index}
+                index={index}
+                todo={todo} />
+                ))}
+         </div>
     </div>
   );
 }
